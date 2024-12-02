6e7000ff6760f0dd18944d664e10391a0036210d
eray <alieraykt@gmail.com>
2024-12-02 18:57:46 +0300

    rapier anims + attack animations

--- Assets/attack anim/AttackController.cs
+++ Assets/attack anim/AttackController.cs
@@ -7,10 +7,13 @@ using UnityEngine.UI;
 
 public class AttackController : MonoBehaviour
 {
-    public TwoBoneIKConstraint _twoBoneIK; // Two Bone IK Constraint bileşeni
+    public TwoBoneIKConstraint _twoBoneIK;
+    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _hint;
+    
     private Animator _animator;
-
-    [SerializeField] private List<Image> _attackWays;
+    
+    [SerializeField] private List<AttackWay> _attackWays ;
 
     private Vector2 _mouseDelta;
     private string _currentWay = "";
@@ -32,9 +35,7 @@ public class AttackController : MonoBehaviour
             if (_mouseDelta.magnitude < 0.05f) return; // Küçük hareketleri yok say
         
             _accumulatedDelta += _mouseDelta;
-
-            Debug.Log($"Accumulated Delta: {_accumulatedDelta}");
-
+            
             // Yön tayini
             if (Mathf.Abs(_accumulatedDelta.x) > Mathf.Abs(_accumulatedDelta.y))
             {
@@ -50,16 +51,13 @@ public class AttackController : MonoBehaviour
             // Birikimi yavaşça sıfırla (yumuşak sıfırlama)
             _accumulatedDelta = Vector2.Lerp(_accumulatedDelta, Vector2.zero, Time.deltaTime * 2);
             
-            
-            
-            // IK ağırlığını artır (kol kalksın)
-            DOVirtual.Float(_twoBoneIK.weight, 1f, 0.5f, val => _twoBoneIK.weight = val);
         }
 
         else if (Input.GetMouseButtonUp(0))
         {
             _accumulatedDelta = Vector2.zero;
-            _animator.SetTrigger("attack");
+            _animator.SetTrigger(_currentWay == "" ? "right" : _currentWay);
+            _attackWays.ForEach(x => x._indicator.gameObject.SetActive(false));
             DOVirtual.Float(_twoBoneIK.weight, 0f, 0.5f, val => _twoBoneIK.weight = val);
         }
 
@@ -69,33 +67,38 @@ public class AttackController : MonoBehaviour
     private void AttackWay(string way)
     {
         if (_currentWay.Equals(way)) return;
-        _attackWays.ForEach(x => x.gameObject.SetActive(false));
-        _currentWay = way;
-
-        switch (way)
+        if (way.Equals(""))
         {
-            case "right":
-                _attackWays[1].gameObject.SetActive(true);
-
-                break;
-
-            case "left":
-                _attackWays[0].gameObject.SetActive(true);
-
-                break;
-            case "up":
-                _attackWays[2].gameObject.SetActive(true);
-
-                break;
-            case "down":
-                _attackWays[3].gameObject.SetActive(true);
-
-                break;
+            _currentWay = "";
+            return;
         }
+        
+        _attackWays.ForEach(x => x._indicator.gameObject.SetActive(false));
+        _currentWay = way;
+        var newAttackWay = _attackWays.Find(x => x._name.Equals(way));
+        newAttackWay._indicator.gameObject.SetActive(true);
+        
+        _target.DOLocalMove(newAttackWay._target.localPosition, 0.5f);
+        _target.DOLocalRotateQuaternion(newAttackWay._target.localRotation, 0.5f);
+        _hint.DOLocalMove(newAttackWay._hint.localPosition, 0.5f);
+        _hint.DOLocalRotateQuaternion(newAttackWay._hint.localRotation, 0.5f);
+        
+        
+        DOVirtual.Float(_twoBoneIK.weight, 1f, 0.5f, val => _twoBoneIK.weight = val);
+
     }
 
     public void Hit()
     {
         Debug.Log("Hit animation event triggered!");
     }
+}
+
+[System.Serializable]
+public class AttackWay
+{
+    public string _name;
+    public Transform _target;
+    public Transform _hint;
+    public Image _indicator;
 }
