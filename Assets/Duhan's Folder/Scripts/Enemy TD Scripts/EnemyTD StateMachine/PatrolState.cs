adb1a4e414e285995810ec6db44243d1e52817a9
Duhan Avcı <avciduhan@gmail.com>
2024-12-15 19:12:42 +0300

    Revert "blabla"
    This reverts commit 54a8a280fe1fed3c7cf11e969f45e1ad449c52f9.

--- Assets/Duhan's Folder/Scripts/Enemy TD Scripts/EnemyTD StateMachine/PatrolState.cs
+++ Assets/Duhan's Folder/Scripts/Enemy TD Scripts/EnemyTD StateMachine/PatrolState.cs
@@ -1,78 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.VersionControl.Asset;
-using UnityEngine.AI;
 
 namespace MountAndBlade
 {
-    public class PatrolState : IState
+    public class PatrolState : MonoBehaviour
     {
-        private NavMeshAgent agent;
-        private Transform enemyTransform;
-        private Vector3 boundsMin;
-        private Vector3 boundsMax;
-        private bool canGeneratePos = true;
-
-        public PatrolState(NavMeshAgent agent, Transform enemyTransform, Vector3 boundsMin, Vector3 boundsMax)
+        // Start is called before the first frame update
+        void Start()
         {
-            this.agent = agent;
-            this.enemyTransform = enemyTransform;
-            this.boundsMin = boundsMin;
-            this.boundsMax = boundsMax;
+        
         }
 
-        public void Enter()
+        // Update is called once per frame
+        void Update()
         {
-            // Baþlangýçta yapýlacak iþlemler
-        }
-
-        public void Update()
-        {
-            PatrolBehaviour();
-        }
-
-        public void Exit()
-        {
-            // Durumdan çýkarken yapýlacak iþlemler
-        }
-
-        private void PatrolBehaviour()
-        {
-            if (canGeneratePos)
-                enemyTransform.GetComponent<EnemyHandler>().StartCoroutine(GenerateRandomPosition());
-
-            // Durum geçiþlerini kontrol et
-            var handler = enemyTransform.GetComponent<EnemyHandler>();
-            if (handler.isInReach && handler.isPlayerStronger)
-                handler.ChangeState(States.chase);
-            else if (handler.isInReach && !handler.isPlayerStronger)
-                handler.ChangeState(States.retreat);
-        }
-
-        private IEnumerator GenerateRandomPosition()
-        {
-            var handler = enemyTransform.GetComponent<EnemyHandler>();
-            handler.canGeneratePos = false;
-            Vector3 randomPosition = GetRandomPositionWithinBounds();
-
-            if (NavMesh.SamplePosition(randomPosition, out NavMeshHit hit, 2f, NavMesh.AllAreas))
-            {
-                agent.SetDestination(hit.position);
-                yield return new WaitUntil(() => !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance);
-                yield return new WaitForSeconds(3f);
-            }
-
-            handler.canGeneratePos = true;
-        }
-
-        private Vector3 GetRandomPositionWithinBounds()
-        {
-            float randomX = Random.Range(boundsMin.x, boundsMax.x);
-            float randomZ = Random.Range(boundsMin.z, boundsMax.z);
-            float fixedY = agent.transform.position.y;
-
-            return new Vector3(randomX, fixedY, randomZ);
+        
         }
     }
 }
