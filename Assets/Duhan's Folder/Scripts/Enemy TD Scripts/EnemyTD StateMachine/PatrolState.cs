992a409c2fee40f61fc3333c00e65524b9600cd8
Duhan Avcı <avciduhan@gmail.com>
2024-12-15 18:59:41 +0300

    Düzenlemeye Başlamak

---
+++ Assets/Duhan's Folder/Scripts/Enemy TD Scripts/EnemyTD StateMachine/PatrolState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MountAndBlade
+{
+    public class PatrolState : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
