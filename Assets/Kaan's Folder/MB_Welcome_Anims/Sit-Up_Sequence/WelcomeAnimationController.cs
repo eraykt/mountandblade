3060adc54eb9e27ce01c2941e41356f6a9c97d3e
Kaan Avdan <kaanavdan01@gmail.com>
2025-05-15 23:02:08 +0300

    MainMenu is OK

---
+++ Assets/Kaan's Folder/MB_Welcome_Anims/Sit-Up_Sequence/WelcomeAnimationController.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MountAndBlade
+{
+    public class WelcomeAnimationController : MonoBehaviour
+    {
+        private Animator _animator;
+        private string SceneToLoad;
+
+        [Header("Buttons")]
+        public Button Button_StartGame;
+        public Button Button_Lore;
+        public Button Button_Credits;
+
+        [Header("Bools")]
+        public bool StartGame = false;
+        public bool ButtonClicked = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+            StartCoroutine(InitCoroutine());
+
+            // Listeners
+            Button_StartGame.onClick.AddListener(Func_StartGame);
+            Button_Lore.onClick.AddListener(Func_Lore);
+            Button_Credits.onClick.AddListener(Func_Credits);
+        }
+
+        private void Func_StartGame()
+        {
+            ButtonClicked = true;
+
+            SceneToLoad = "02_MapScene";
+        }
+
+        private void Func_Lore()
+        {
+            ButtonClicked = true;
+
+            // SceneToLoad = LORE SAHNESI
+        }
+
+        private void Func_Credits()
+        {
+            ButtonClicked = true;
+
+            SceneToLoad = "00_Credits";
+        }
+
+        private IEnumerator InitCoroutine()
+        {
+            yield return new WaitForSeconds(3);
+            AnimParameterReseter();
+            _animator.SetBool("IdleToSitup", true);
+        }
+
+        public void EndIdleToSitup()
+        {
+            AnimParameterReseter();
+            _animator.SetBool("Situp", true);
+        }
+
+        private void Update()
+        {
+            if (ButtonClicked)
+            {
+                AnimParameterReseter();
+                _animator.SetBool("SitupToIdle", true);
+            }
+        }
+
+        public void EndSitupToIdle()
+        {
+            AnimParameterReseter();
+            _animator.SetBool("Idle", true);
+            StartCoroutine(TransitionCoroutine());
+        }
+
+        private void AnimParameterReseter()
+        {
+            _animator.SetBool("Idle", false);
+            _animator.SetBool("IdleToSitup", false);
+            _animator.SetBool("Situp", false);
+            _animator.SetBool("SitupToIdle", false);
+
+        }
+
+        private IEnumerator TransitionCoroutine()
+        {
+            yield return new WaitForSeconds(2);
+            SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+}
